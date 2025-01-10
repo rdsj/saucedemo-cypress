0902a570a6cfd63b1fdee7c0204088f7a63d6fb9
Reginaldo Júnior <reginaldojr.rdsj@gmail.com>
2025-01-10 00:31:24 -0300

    Update compra.cy.js

--- cypress/e2e/compra.cy.js
+++ cypress/e2e/compra.cy.js
@@ -1,26 +1,23 @@
 
+import LoginPage from '../pages/login'
 import compraPage from '../pages/compra'
+import produtosPage from '../pages/carrinho'
 
 
 describe('Concluir compra', () => {
+
+    beforeEach(() => {
+        LoginPage.acessarPagina()
+        LoginPage.loginValido()
+    })  
     
 
     it('Concluir compra', () => {
 
-        cy.visit('https://www.saucedemo.com/')  
-        cy.get('[data-test="username"]').type('standard_user');
-        cy.get('[data-test="password"]').type('secret_sauce');  
-        cy.get('[data-test="login-button"]').click();  
-        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();  
-        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-        cy.get('.shopping_cart_link').should('be.visible').and('have.text', '2');
-        cy.get('[data-test="shopping-cart-link"]').click();
-         cy.url().should('eq', 'https://www.saucedemo.com/cart.html');
-
-
-compraPage.checkout(); 
-compraPage.form('Joao', 'Silva', '12345');
-compraPage.finish();    
+        produtosPage.adicionarAoCarrinho('Sauce-Labs-Backpack');
+        compraPage.checkout(); 
+        compraPage.form('Joao', 'Silva', '12345');
+        compraPage.finish();    
        
         
          
